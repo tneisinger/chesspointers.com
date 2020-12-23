6870de124554dfed8ce9bfad7a5c4c67f19016c9
Tyler Neisinger <tjneisi@gmail.com>
2020-12-23 09:36:42 -0800

    Add more tests for ChessNavBtns

--- services/node/src/client/components/ChessNavBtns.test.tsx
+++ services/node/src/client/components/ChessNavBtns.test.tsx
@@ -30,3 +30,61 @@ describe('<ChessNavBtns atStart={true} atEnd={false} ... />', () => {
     expect(wrapper.find('[aria-label="jump to end"]').prop('disabled')).toBe(false);
   });
 });
+
+describe('<ChessNavBtns atStart={false} atEnd={true} ... />', () => {
+  const wrapper = shallow(
+    <ChessNavBtns
+      atStart={false}
+      atEnd={true}
+      jumpToStart={() => {}}
+      jumpToEnd={() => {}}
+      stepForward={() => {}}
+      stepBack={() => {}}
+    />
+  );
+
+  it('should have an enabled jumpToStart button', () => {
+    expect(wrapper.find('[aria-label="jump to start"]').prop('disabled')).toBe(false);
+  });
+
+  it('should have an enabled back button', () => {
+    expect(wrapper.find('[aria-label="back"]').prop('disabled')).toBe(false);
+  });
+
+  it('should have a disabled forward button', () => {
+    expect(wrapper.find('[aria-label="forward"]').prop('disabled')).toBe(true);
+  });
+
+  it('should have a disabled jumpToEnd button', () => {
+    expect(wrapper.find('[aria-label="jump to end"]').prop('disabled')).toBe(true);
+  });
+});
+
+describe('<ChessNavBtns atStart={false} atEnd={false} ... />', () => {
+  const wrapper = shallow(
+    <ChessNavBtns
+      atStart={false}
+      atEnd={false}
+      jumpToStart={() => {}}
+      jumpToEnd={() => {}}
+      stepForward={() => {}}
+      stepBack={() => {}}
+    />
+  );
+
+  it('should have an enabled jumpToStart button', () => {
+    expect(wrapper.find('[aria-label="jump to start"]').prop('disabled')).toBe(false);
+  });
+
+  it('should have an enabled back button', () => {
+    expect(wrapper.find('[aria-label="back"]').prop('disabled')).toBe(false);
+  });
+
+  it('should have an enabled forward button', () => {
+    expect(wrapper.find('[aria-label="forward"]').prop('disabled')).toBe(false);
+  });
+
+  it('should have an enabled jumpToEnd button', () => {
+    expect(wrapper.find('[aria-label="jump to end"]').prop('disabled')).toBe(false);
+  });
+});
