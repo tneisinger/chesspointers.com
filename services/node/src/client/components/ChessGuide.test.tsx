f519be7e7f53bf5d3b47435c89695b6b4a9bd45e
Tyler Neisinger <tjneisi@gmail.com>
2020-12-23 14:08:14 -0800

    Add react-testing-library and add two tests

--- services/node/src/client/components/ChessGuide.test.tsx
+++ services/node/src/client/components/ChessGuide.test.tsx
@@ -1,20 +1,42 @@
 import React from 'react';
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect'
 import { shallow } from 'enzyme';
 import Chessboard from "chessboardjsx";
 import ChessGuide from './ChessGuide';
 import { ChessSequence } from '../types/chess';
 
-describe('<ChessGuide /> with basic chessSequence', () => {
-  const basicChessSequence: ChessSequence = {
+// This speeds up tests that include `waitFor`
+jest.useFakeTimers();
+
+describe('<ChessGuide /> with simple chessSequence', () => {
+  const firstMoveComment = 'This is the first move';
+  const simpleChessSequence: ChessSequence = {
     endsInCheckmate: false,
     isPlayedByWhite: true,
-    moves: [],
-    finalComment: '',
+    moves: [
+      { move: 'e4',
+        comment: firstMoveComment,
+      },
+      { move: 'e5' },
+      { move: 'Nf3' },
+      { move: 'Nc6' },
+    ],
+    finalComment: ''
   }
 
-  const chessGuide = shallow(<ChessGuide chessSequence={basicChessSequence} />);
-
   it('should have a Chessboard component', () => {
+    const chessGuide = shallow(<ChessGuide chessSequence={simpleChessSequence} />);
     expect(chessGuide.find(Chessboard).length).toEqual(1);
+  });
+
+  it('should show the welcome comment', () => {
+    render(<ChessGuide chessSequence={simpleChessSequence} />);
+    expect(screen.getByText("Welcome!")).toBeInTheDocument();
+  });
+
+  it('should show the first comment after waiting two seconds', async () => {
+    render(<ChessGuide chessSequence={simpleChessSequence} />);
+    await waitFor(() => screen.getByText(firstMoveComment), { timeout: 2000 });
   });
 });
