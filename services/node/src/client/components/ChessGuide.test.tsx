ab1c08bdcb9dc705734fdda274dd1c9aaf8ecc42
Tyler Neisinger <tjneisi@gmail.com>
2020-12-24 09:30:47 -0800

    Add more tests for ChessGuide component

--- services/node/src/client/components/ChessGuide.test.tsx
+++ services/node/src/client/components/ChessGuide.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { render, waitFor } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect'
+import { render, waitFor, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
 import ChessGuide, { HIGHLIGHTED_SQUARE_BOX_SHADOW } from './ChessGuide';
 import { ChessSequence } from '../types/chess';
 
 const highlightSquareStyle = `box-shadow: ${HIGHLIGHTED_SQUARE_BOX_SHADOW}`;
 
-// This speeds up tests that include `waitFor`
 jest.useFakeTimers();
 
 describe('<ChessGuide /> with simple chessSequence', () => {
@@ -67,4 +67,56 @@ describe('<ChessGuide /> with simple chessSequence', () => {
       expect(e4Square.firstChild).toHaveStyle(highlightSquareStyle);
     }, { timeout: 2000 });
   });
+
+  it('should play first move when stepForwardBtn clicked', () => {
+    const { container, getByTestId } =
+      render(<ChessGuide chessSequence={simpleChessSequence} />);
+    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
+    fireEvent.click(stepForwardBtn);
+    getByTestId('wP-e4');
+  });
+
+  it('should not play computer move when stepForwardBtn clicked', () => {
+    const { container, getByTestId } =
+      render(<ChessGuide chessSequence={simpleChessSequence} />);
+    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
+    fireEvent.click(stepForwardBtn);
+    getByTestId('wP-e4');
+    expect(() => getByTestId('bP-e5')).toThrow();
+  });
+
+  it('should play computer move when stepForwardBtn clicked second time', () => {
+    const { container, getByTestId } =
+      render(<ChessGuide chessSequence={simpleChessSequence} />);
+    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
+    fireEvent.click(stepForwardBtn);
+    fireEvent.click(stepForwardBtn);
+    getByTestId('bP-e5');
+  });
+
+  // Note: I haven't been able to get tests to perform drag-and-drop events,
+  // so I'm giving up on that for now.
+  // Below is my last failed attempt to drag-and-drop in a test.
+  /*
+  it('should play move after click and drag', async () => {
+    const { container, getByTestId } =
+      render(<ChessGuide chessSequence={simpleChessSequence} />);
+    const e2Pawn = getByTestId("wP-e2");
+    const e4Square = container.querySelector('[data-squareid="e4"]');
+    const dataTransfer = {
+      dropEffect: 'move',
+      effectAllowed: 'move',
+      items: [e2Pawn],
+      types: 'move',
+    };
+    fireEvent.dragStart(e2Pawn, { dataTransfer });
+    fireEvent.dragEnter(e4Square, { dataTransfer });
+    fireEvent.dragOver(e4Square, { dataTransfer });
+    fireEvent.drop(e4Square, { dataTransfer });
+
+    await waitFor(() => {
+      getByTestId("wP-e4");
+    });
+  });
+  */
 });
