6facdf1bfed8727cc23c0960facca4355ca2af61
Tyler Neisinger <tjneisi@gmail.com>
2020-12-26 10:30:08 -0800

    Remove comment-related tests

--- services/node/src/client/components/ChessGuide.test.tsx
+++ services/node/src/client/components/ChessGuide.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitFor, fireEvent, act } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import ChessGuide, {
@@ -67,24 +67,6 @@ describe('<ChessGuide /> with simple chessSequence', () => {
       />
     );
     expect(container.querySelector('[data-boardid="0"]')).not.toBeNull();
-  });
-
-  it('should show the welcome comment', () => {
-    const { getByText } = render(
-      <ChessGuide
-        chessSequence={simpleChessSequence}
-      />
-    );
-    expect(getByText("Welcome!")).toBeInTheDocument();
-  });
-
-  it('should show the first move comment after short wait', async () => {
-    const { getByText } = render(
-      <ChessGuide
-        chessSequence={simpleChessSequence}
-      />
-    );
-    await waitFor(() => getByText(firstMoveComment));
   });
 
   it('should not highlight first move initially', () => {
@@ -160,19 +142,6 @@ describe('<ChessGuide /> with simple chessSequence', () => {
     }, { timeout: 1000 });
   });
 
-  it('shows third move comment when stepForwardBtn clicked three times', async () => {
-    const { container, getByText } =
-      render(
-        <ChessGuide
-          chessSequence={simpleChessSequence}
-        />
-      );
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    fireEvent.click(stepForwardBtn);
-    await waitFor(() => getByText(thirdMoveComment));
-  });
-
   it('highlights third move after stepForwardBtn clicked twice', async () => {
     const { container } =
       render(
@@ -187,58 +156,4 @@ describe('<ChessGuide /> with simple chessSequence', () => {
       expectSquaresToBeHighlighted(container, ['g1', 'f3'])
     );
   });
-
-  it('removes old comment if no new comment after stepBack', () => {
-    const { container, queryByText } =
-      render(
-        <ChessGuide
-          chessSequence={simpleChessSequence}
-        />
-      );
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    const stepBackBtn = container.querySelector('[aria-label="step back"]');
-    act(() => {
-      fireEvent.click(stepForwardBtn);
-      return undefined;
-    });
-    act(() => {
-      fireEvent.click(stepForwardBtn);
-      return undefined;
-    });
-    act(() => {
-      fireEvent.click(stepBackBtn);
-      return undefined;
-    });
-    act(() => {
-      jest.advanceTimersByTime(3000);
-      return undefined;
-    });
-    expect(queryByText(thirdMoveComment)).toBeNull();
-  });
-
-  // Note: I haven't been able to get tests to perform drag-and-drop events,
-  // so I'm giving up on that for now.
-  // Below is my last failed attempt to drag-and-drop in a test.
-  /*
-  it('should play move after click and drag', async () => {
-    const { container, getByTestId } =
-      render(<ChessGuide chessSequence={simpleChessSequence} />);
-    const e2Pawn = getByTestId("wP-e2");
-    const e4Square = container.querySelector('[data-squareid="e4"]');
-    const dataTransfer = {
-      dropEffect: 'move',
-      effectAllowed: 'move',
-      items: [e2Pawn],
-      types: 'move',
-    };
-    fireEvent.dragStart(e2Pawn, { dataTransfer });
-    fireEvent.dragEnter(e4Square, { dataTransfer });
-    fireEvent.dragOver(e4Square, { dataTransfer });
-    fireEvent.drop(e4Square, { dataTransfer });
-
-    await waitFor(() => {
-      getByTestId("wP-e4");
-    });
-  });
-  */
 });
