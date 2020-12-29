62b93aae517b0253019ed76e51c56b1626605d07
Tyler Neisinger <tjneisi@gmail.com>
2020-12-29 15:19:28 -0800

    Add arraysEqual() to utils

--- services/node/src/shared/utils.ts
+++ services/node/src/shared/utils.ts
@@ -3,3 +3,8 @@ import { User } from './entity/user';
 export function getUserFullName(user: User): string {
   return `${user.firstName} ${user.lastName}`;
 }
+
+export function arraysEqual<T>(a: T[], b: T[]): boolean {
+  return a.length === b.length &&
+    a.every((val, idx) => val === b[idx]);
+}
