7ed1b0d6ead66e97536c441e1246d22eb4af2293
Tyler Neisinger <tjneisi@gmail.com>
2021-01-03 12:48:01 -0800

    Add three array utility functions to utils

--- services/node/src/shared/utils.ts
+++ services/node/src/shared/utils.ts
@@ -8,3 +8,17 @@ export function arraysEqual<T>(a: T[], b: T[]): boolean {
   return a.length === b.length &&
     a.every((val, idx) => val === b[idx]);
 }
+
+export function getArrayDiff<T>(setA: T[], setB: T[]): T[] {
+  return setA.filter((a) => !setB.includes(a));
+}
+
+export function partition<T>(array: T[], isValid: (t: T) => boolean): T[][] {
+  return array.reduce(([pass, fail], elem) => {
+    return isValid(elem) ? [[...pass, elem], fail] : [pass, [...fail, elem]];
+  }, [[], []]);
+}
+
+export function randomElem<T>(array: T[]): (T | undefined) {
+  return array[Math.floor(Math.random() * array.length)];
+}
