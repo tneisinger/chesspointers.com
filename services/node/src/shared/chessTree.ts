cb99f446bc0f1e45e03f026d1b17b827bf6b7d6f
Tyler Neisinger <tjneisi@gmail.com>
2021-01-13 22:08:51 -0800

    Rewrite 'mergeTrees' function to avoid recursion
    After the previous commit, I was getting errors about exceeding max
    recursion depth. I fixed that problem by rewriting the 'mergeTrees'
    function without using recursion.

--- services/node/src/shared/chessTree.ts
+++ services/node/src/shared/chessTree.ts
@@ -1,6 +1,6 @@
 import { Chess } from "chess.js";
 import { ChessTree } from './chessTypes';
-import { areChessMovesEquivalent } from './utils';
+import { areChessMovesEquivalent, areChessPathsEquivalent } from './utils';
 
 export const makeChessTree = (moves: string[], childTrees: ChessTree[]): ChessTree => {
   let result: ChessTree = { move: '', children: []};
@@ -73,39 +73,72 @@ export const validateChessTree = (
   });
 }
 
-export function mergeTrees(...trees: ChessTree[]): ChessTree {
-  return trees.reduce((acc, tree) => {
-    return mergeTwoTrees(acc, tree);
-  }, { move: '', children: []})
-}
-
-export function mergeTwoTrees(tree1: ChessTree, tree2: ChessTree): ChessTree {
-  if (tree1.move === '' && tree2.move === '') {
-    return {
-      move: '',
-      children: [ ...tree1.children, ...tree2.children ]
-    };
-  } else if (tree1.move === '') {
-    return tree1.children.reduce((acc, childTree) => {
-      return mergeTwoTrees(acc, childTree);
-    }, tree2);
-  } else if (tree2.move === '') {
-    return tree2.children.reduce((acc, childTree) => {
-      return mergeTwoTrees(acc, childTree);
-    }, tree1);
-  }
-
-  if (areChessMovesEquivalent(tree1.move, tree2.move)) {
-    const childTree = mergeTrees(...tree1.children, ...tree2.children);
-    const children = childTree.move === '' ? childTree.children : [childTree];
-    return {
-      move: tree1.move,
-      children
+function getSubtreeAtPath(path: string[], tree: ChessTree): ChessTree {
+  if (path.length === 0) return tree;
+  if (tree.move === '') {
+    const move = path[0];
+    const childrenWithNextMove =
+      tree.children.filter(child => areChessMovesEquivalent(child.move, move));
+    if (childrenWithNextMove.length > 1) {
+      throw new Error(`Found multiple children with same move: ${move}`);
+    } else if (childrenWithNextMove.length < 1) {
+      throw new Error(`Move ${move} not found`);
+    } else {
+      return getSubtreeAtPath(path, childrenWithNextMove[0]);
     }
   } else {
-    return {
-      move: '',
-      children: [ tree1, tree2 ]
+    if (areChessMovesEquivalent(path[0], tree.move)) {
+      return getSubtreeAtPath(path.slice(1), {move: '', children: tree.children});
+    } else {
+      throw new Error(`Move ${path[0]} did not match next tree move ${tree.move}`);
     }
   }
+}
+
+export function mergeTrees(...trees: ChessTree[]): ChessTree {
+  // Put all the paths of all the trees into one list
+  const paths: string[][] = [];
+  trees.forEach(tree => getUniquePaths(tree).forEach(path => paths.push(path)));
+
+  // If there are no paths, just return an empty tree
+  if (paths.length <= 0) {
+    return { move: '', children: [] };
+  }
+
+  const result = {move: '', children: []};
+  const lengthOfLongestPath = Math.max(...paths.map(path => path.length));
+
+  // Each iteration of this for-loop represents one step deeper into what will
+  // be the resulting ChessTree. Upon each step down, add all the nodes that should
+  // exist at that level before going further down into the tree. (Breadth first)
+  for (let idx = 0; idx < lengthOfLongestPath; idx++) {
+    const pathsToAdd: string[][] = [];
+
+    paths.forEach(path => {
+      const pathSoFar = path.slice(0, idx+1);
+      // if the current path has a move at this depth and if this 'pathSoFar' hasn't
+      // already been added to the list of 'pathsToAdd', push it onto 'pathsToAdd'.
+      if ( path[idx] != undefined &&
+           !pathsToAdd.some(p => areChessPathsEquivalent(p, pathSoFar))
+         ) {
+        pathsToAdd.push(pathSoFar);
+      }
+    });
+    pathsToAdd.forEach(path => {
+      const pathBeforeMove = path.slice(0,-1);
+      const move = path[path.length - 1];
+
+      // Attach a new node to the result tree
+      getSubtreeAtPath(pathBeforeMove, result).children.push({move, children: []});
+    });
+  }
+
+  // At this point, the 'result' ChessTree will always have a root node with an empty
+  // string as its move. In this case, if there is only one child node attached to the
+  // root node, then we can just throw away the root node and just return the child node.
+  if (result.move === '' && result.children.length === 1) {
+    return result.children[0];
+  }
+
+  return result;
 }
