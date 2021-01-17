5abb6652d59272f597888de71a2a23fc283ee189
Tyler Neisinger <tjneisi@gmail.com>
2021-01-17 15:19:26 -0800

    Make links on ChessTraps page not so ugly

--- services/node/src/client/components/ChessTrapsList.tsx
+++ services/node/src/client/components/ChessTrapsList.tsx
@@ -1,15 +1,33 @@
 import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import List from '@material-ui/core/List';
+import { makeStyles } from '@material-ui/core';
 import ListItem from '@material-ui/core/List';
+import Typography from '@material-ui/core/Typography';
 import { useSelector, useDispatch } from 'react-redux';
 import { RootState } from '../redux/store';
 import { getChessTrapsThunk } from '../redux/chessTrapsSlice';
 import { toDashedLowercase } from '../../shared/utils';
 import { formatTrapName } from '../../shared/chessTraps/index';
 
-const ChessTrapsList = () => {
+const useStyles = makeStyles(() => ({
+  navLink: {
+    color: 'white',
+    textDecoration: 'none',
+    '&:visited': {
+      color: '#d6d8de'
+    },
+    '&:hover': {
+      textDecoration: 'underline',
+    }
+  },
+}));
+
+const ChessTrapsList: React.FC<void> = () => {
+  const classes = useStyles();
+
   const dispatch = useDispatch();
+
   const chessTrapsSlice = useSelector((state: RootState) => state.chessTrapsSlice);
 
   useEffect(() => {
@@ -34,11 +52,15 @@ const ChessTrapsList = () => {
     <List>
       { chessTrapsSlice.traps.map(trap => {
         return (
-          <ListItem key={trap.name}>
-            <NavLink to={`/traps/${toDashedLowercase(trap.name)}`} >
-              {formatTrapName(trap)}
-            </NavLink>
-          </ListItem>
+          <Typography>
+            <ListItem key={trap.name}>
+              <NavLink
+                className={classes.navLink}
+                to={`/traps/${toDashedLowercase(trap.name)}`} >
+                {formatTrapName(trap)}
+              </NavLink>
+            </ListItem>
+          </Typography>
         );
       })}
     </List>
