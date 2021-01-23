b2e4a52f42240de8652a4a15cb80b44fcd32d964
Tyler Neisinger <tjneisi@gmail.com>
2021-01-23 07:57:25 -0800

    Add calcChessBoardSize function to utils

--- services/node/src/client/utils/index.ts
+++ services/node/src/client/utils/index.ts
@@ -1,3 +1,41 @@
 export function viewportHeight(): number {
   return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
 }
+
+export function viewportWidth(): number {
+  return Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+}
+
+export function vmin(): number {
+  return Math.min(viewportHeight(), viewportWidth());
+}
+
+export function vmax(): number {
+  return Math.max(viewportWidth(), viewportHeight());
+}
+
+export type ViewportUnits = 'vh' | 'vw' | 'vmin' | 'vmax';
+
+
+export function calcChessBoardSize(value: number, units: ViewportUnits): number {
+  let pixels = 0;
+  switch (units) {
+    case 'vh':
+      pixels = (viewportHeight() * value) / 100;
+      break;
+    case 'vw':
+      pixels = (viewportWidth() * value) / 100;
+      break;
+    case 'vmin':
+      pixels = (vmin() * value) / 100;
+      break;
+    case 'vmax':
+      pixels = (vmax() * value) / 100;
+      break;
+  }
+  // The final board size of a Chessground chessboard needs to be some multiple of eight
+  // pixels. See here: https://github.com/ornicar/chessground/issues/51
+  const roundValue = 8;
+  const roundedPixels = Math.floor(pixels / roundValue) * roundValue;
+  return roundedPixels;
+}
