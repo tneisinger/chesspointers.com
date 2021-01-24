3e385f9712b19cb52b258f7879e801529a4ce905
Tyler Neisinger <tjneisi@gmail.com>
2021-01-24 14:08:19 -0800

    Make links on 'Chess Traps' page work again

--- services/node/src/client/components/DisplayChessTraps.tsx
+++ services/node/src/client/components/DisplayChessTraps.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import Typography from '@material-ui/core/Typography'
+import { NavLink }  from 'react-router-dom'
 import { ChessTrap } from '../../shared/entity/chessTrap';
 import { formatTrapName } from '../../shared/chessTraps/index';
+import { toDashedLowercase } from '../../shared/utils';
 
 interface Props {
   chessTraps: ChessTrap[]
@@ -10,7 +13,11 @@ const DisplayChessTraps: React.FC<Props> = ({ chessTraps }) => {
   return (
     <>
       {chessTraps.map(trap => (
-        <p>{formatTrapName(trap)}</p>
+        <Typography>
+          <NavLink key={trap.name} to={`/traps/${toDashedLowercase(trap.name)}`}>
+            {formatTrapName(trap)}
+          </NavLink>
+        </Typography>
       ))}
     </>
   );
