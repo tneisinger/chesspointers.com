041fddec53e76c32479c65d0a5aef646647576e1
Tyler Neisinger <tjneisi@gmail.com>
2021-01-24 19:26:35 -0800

    Use new ChessLessonPreview on Chess Traps page

---
+++ services/node/src/client/components/ChessLessonPreview.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import ChessTreePreview from './ChessTreePreview';
+import { ChessTree, PieceColor } from '../../shared/chessTypes';
+
+interface Props {
+  chessTree: ChessTree;
+  orientation: PieceColor;
+  title: string;
+}
+
+const ChessLessonPreview: React.FC<Props> = (props) => {
+
+  return (
+    <Grid
+      container
+      direction='column'
+      justify='center'
+      align-items='center'
+      spacing={2}
+    >
+      <Grid item>
+        <Typography
+          variant='h5'
+          component='h4'
+          align='center'
+        >
+          {props.title}
+        </Typography>
+      </Grid>
+      <Grid item>
+        <Grid container direction='row' xs={12} justify='center'>
+          <Grid item>
+            <ChessTreePreview
+              chessTree={props.chessTree}
+              orientation={props.orientation}
+            />
+          </Grid>
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+}
+
+export default ChessLessonPreview;
--- services/node/src/client/components/DisplayChessTraps.tsx
+++ services/node/src/client/components/DisplayChessTraps.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import ChessLessonPreview from './ChessLessonPreview';
 import { ChessTrap } from '../../shared/entity/chessTrap';
 import { formatTrapName } from '../../shared/chessTraps/index';
+import { toDashedLowercase } from '../../shared/utils';
 
 interface Props {
   chessTraps: ChessTrap[]
@@ -8,11 +12,21 @@ interface Props {
 
 const DisplayChessTraps: React.FC<Props> = ({ chessTraps }) => {
   return (
-    <>
+    <Grid container direction='column' spacing={6}>
       {chessTraps.map(trap => (
-        <p>{formatTrapName(trap)}</p>
+        <Grid key={trap.name} item>
+          <NavLink
+            to={`/traps/${toDashedLowercase(trap.name)}`}
+          >
+            <ChessLessonPreview
+              chessTree={trap.chessTree}
+              orientation={trap.playedByWhite ? 'white' : 'black'}
+              title={formatTrapName(trap)}
+            />
+          </NavLink>
+        </Grid>
       ))}
-    </>
+    </Grid>
   );
 }
 
