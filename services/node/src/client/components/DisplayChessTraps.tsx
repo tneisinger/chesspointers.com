a214d308662c6b4a2ace6eebe682bf9e34db1675
Tyler Neisinger <tjneisi@gmail.com>
2021-01-28 13:18:49 -0800

    Add filtering and change layout of '/traps' page

--- services/node/src/client/components/DisplayChessTraps.tsx
+++ services/node/src/client/components/DisplayChessTraps.tsx
@@ -1,30 +1,46 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
-import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
 import ChessLessonPreview from './ChessLessonPreview';
 import { ChessTrap } from '../../shared/entity/chessTrap';
 import { formatTrapName } from '../../shared/chessTraps/index';
 import { toDashedLowercase } from '../../shared/utils';
+
+const useStyles = makeStyles({
+  card: {
+    display: 'inline-block',
+    margin: '16px',
+  },
+  cardContent: {
+    padding: '24px',
+  },
+});
 
 interface Props {
   chessTraps: ChessTrap[];
 }
 
 const DisplayChessTraps: React.FC<Props> = ({ chessTraps }) => {
+  const classes = useStyles({});
+
   return (
-    <Grid container direction='column' spacing={6}>
+    <>
       {chessTraps.map((trap) => (
-        <Grid key={trap.name} item>
-          <NavLink to={`/traps/${toDashedLowercase(trap.name)}`}>
-            <ChessLessonPreview
-              chessTree={trap.chessTree}
-              orientation={trap.playedByWhite ? 'white' : 'black'}
-              title={formatTrapName(trap)}
-            />
-          </NavLink>
-        </Grid>
+        <Card key={trap.name} className={classes.card}>
+          <CardContent className={classes.cardContent}>
+            <NavLink to={`/traps/${toDashedLowercase(trap.name)}`}>
+              <ChessLessonPreview
+                chessTree={trap.chessTree}
+                orientation={trap.playedByWhite ? 'white' : 'black'}
+                title={formatTrapName(trap)}
+              />
+            </NavLink>
+          </CardContent>
+        </Card>
       ))}
-    </Grid>
+    </>
   );
 };
 
--- services/node/src/client/pages/ChessTraps.tsx
+++ services/node/src/client/pages/ChessTraps.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
-import RequireChessTraps from '../components/RequireChessTraps';
-import DisplayAllChessTraps from '../components/DisplayAllChessTraps';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { RootState } from '../redux/store';
+import { getChessTrapsThunk } from '../redux/chessTrapsSlice';
+import Grid from '@material-ui/core/Grid';
+import DisplayChessTraps from '../components/DisplayChessTraps';
+import ChessTrapFilters from '../components/ChessTrapFilters';
+import { ChessTrap } from '../../shared/entity/chessTrap';
 
 const ChessTrapsPage: React.FunctionComponent = () => {
+  const dispatch = useDispatch();
+
+  const chessTrapsSlice = useSelector((state: RootState) => state.chessTrapsSlice);
+
+  const [visibleTraps, setVisibleTraps] = useState<ChessTrap[]>(chessTrapsSlice.traps);
+
+  useEffect(() => {
+    if (chessTrapsSlice.requestStatus === 'NO_REQUEST_YET') {
+      dispatch(getChessTrapsThunk());
+    }
+  }, []);
+
+  if (chessTrapsSlice.requestStatus === 'ERROR') {
+    return <p>An error occurred: {chessTrapsSlice.error}</p>;
+  }
+
+  if (chessTrapsSlice.requestStatus !== 'LOADED') {
+    return <p>Loading...</p>;
+  }
+
   return (
-    <RequireChessTraps>
-      <DisplayAllChessTraps chessTraps={[]} />
-    </RequireChessTraps>
+    <Grid container direction='row' justify='center'>
+      <Grid item xs={3}>
+        <ChessTrapFilters
+          allTraps={chessTrapsSlice.traps}
+          setSelectedTraps={setVisibleTraps}
+        />
+      </Grid>
+      <Grid item xs={9}>
+        <DisplayChessTraps chessTraps={visibleTraps} />
+      </Grid>
+    </Grid>
   );
 };
 
