801ef25757f4227423b6b91c86c28d6b3684aadd
Tyler Neisinger <tjneisi@gmail.com>
2021-01-31 09:45:08 -0800

    Improve chessTrap table and column names
    Replace camelcase table and column names with underscored versions. This
    makes working in psql much more pleasant.

--- services/node/src/shared/entity/chessTrap.ts
+++ services/node/src/shared/entity/chessTrap.ts
@@ -1,13 +1,14 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn } from 'typeorm';
 import { ChessTree } from '../chessTypes';
 
-@Entity({ name: 'chessTraps' })
+@Entity({ name: 'chess_traps' })
 export class ChessTrap {
   @PrimaryGeneratedColumn()
   id: number;
 
   @Column({
     type: 'varchar',
+    name: 'full_name',
     length: 50,
     unique: true,
     nullable: false,
@@ -16,6 +17,7 @@ export class ChessTrap {
 
   @Column({
     type: 'varchar',
+    name: 'short_name',
     length: 30,
     unique: true,
     nullable: false,
@@ -24,17 +26,21 @@ export class ChessTrap {
 
   @Column({
     type: 'bool',
+    name: 'played_by_white',
     nullable: false,
   })
   playedByWhite: boolean;
 
   @Column({
     type: 'jsonb',
+    name: 'chess_tree',
     array: false,
     nullable: false,
   })
   chessTree: ChessTree;
 
-  @CreateDateColumn()
+  @CreateDateColumn({
+    name: 'added_on',
+  })
   addedOn: Date;
 }
