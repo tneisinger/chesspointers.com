79283d7c27fabd2531f1378bfb185ec9c390d08e
Tyler Neisinger <tjneisi@gmail.com>
2021-02-16 11:19:37 -0800

    Remove Auth0Provider
    Auth0 requires the site to be https, which I don't want to worry about
    for now. Remove Auth0 so that the site will work over http.

--- services/node/src/client/client.tsx
+++ services/node/src/client/client.tsx
@@ -1,27 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { Auth0Provider, AppState } from '@auth0/auth0-react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
-import history from './utils/history';
-
-const onRedirectCallback = (appState: AppState) => {
-  history.push(
-    appState && appState.returnTo ? appState.returnTo : window.location.pathname,
-  );
-};
 
 ReactDOM.render(
   <Provider store={store}>
-    <Auth0Provider
-      domain={process.env.AUTH0_DOMAIN as string}
-      clientId={process.env.AUTH0_CLIENTID as string}
-      redirectUri={window.location.origin}
-      onRedirectCallback={onRedirectCallback}
-    >
-      <App />
-    </Auth0Provider>
+    <App />
   </Provider>,
   document.getElementById('app'),
 );
