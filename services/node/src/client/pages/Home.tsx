6de889532f861fdf168d318406304ee7fc85b29d
Tyler Neisinger <tjneisi@gmail.com>
2021-04-06 16:56:09 -0700

    Small changes to homepage

--- services/node/src/client/pages/Home.tsx
+++ services/node/src/client/pages/Home.tsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { makeStyles, CardHeader } from '@material-ui/core';
-import Card from '@material-ui/core/Card';
-import CardContent from '@material-ui/core/CardContent';
+import { makeStyles } from '@material-ui/core';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(() => ({
   mainCard: {
     padding: '0 40px',
+    marginTop: '20px',
   },
   cardHeader: {
     textAlign: 'center',
     paddingBottom: '0',
     marginBottom: '0',
   },
+  heading: {
+    marginBottom: '1rem',
+  }
 }));
 
 const HomePage: React.FunctionComponent = () => {
@@ -20,16 +24,22 @@ const HomePage: React.FunctionComponent = () => {
 
   return (
     <Grid item xs={12}>
-      <Grid container direction='row' justify='center' spacing={2}>
-        <Grid item>
-          <Card className={classes.mainCard}>
-            <CardHeader className={classes.cardHeader} title='Home Page' />
-            <CardContent>
-              <p>Welcome!</p>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
+      <Box p={4}>
+        <Typography
+          className={classes.heading}
+          variant='h4'
+          component='h3'
+          align='center'
+        >
+          Welcome!
+        </Typography>
+        <Typography align='center'>
+          This web app is still a work-in-progress.
+        </Typography>
+        <Typography align='center'>
+          If you find a bug, please send an email to admin@chesspointers.com
+        </Typography>
+      </Box>
     </Grid>
   );
 };
