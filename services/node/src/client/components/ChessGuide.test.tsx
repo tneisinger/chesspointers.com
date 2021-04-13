2eb8a211d2200b249fff3462f5e5f8a31aecb4df
Tyler Neisinger <tjneisi@gmail.com>
2021-04-13 14:26:59 -0700

    Tests: Update ChessGuide test file
    Remove a bunch of outdated tests. Add in a few updated tests to get
    started. More tests need to be added.

--- services/node/src/client/components/ChessGuide.test.tsx
+++ services/node/src/client/components/ChessGuide.test.tsx
@@ -3,43 +3,33 @@ import { render, waitFor, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import '@testing-library/jest-dom/extend-expect';
 import ChessGuide from './ChessGuide';
-import { USER_HIGHLIGHT_SQUARE_STYLE } from './ChessGuideBoard';
+import MovesPane from '../components/MovesPane';
 import { ChessTree } from '../../shared/chessTypes';
 import { makeChessTree } from '../../shared/chessTree';
 import { SELECT_BTN_TEST_ID } from './ChessMoveSelector';
-
-// CSS style for a highlighted square, indicating a possible user move
-const userHighlightSquareStyle = `box-shadow: ${USER_HIGHLIGHT_SQUARE_STYLE}`;
+import { LessonType } from '../../shared/entity/lesson';
 
 jest.useFakeTimers();
 
 // Prevents irrelevant errors
 jest.mock('../beeper');
 
-// Given a ChessGuide component element and a list of squares (e.g. ['e2', 'e4']), expect
-// all of those squares to be highlighted.
-const expectSquaresToBeHighlighted = (container: Element, squares: string[]) => {
-  squares.forEach((square) => {
-    const squareElem = container.querySelector(`[data-squareid="${square}"]`);
-    expect(squareElem.firstChild).toHaveStyle(userHighlightSquareStyle);
-  });
-};
-
-// Same as above, but expect the squares not be highlighted.
-const expectSquaresNotToBeHighlighted = (container: Element, squares: string[]) => {
-  squares.forEach((square) => {
-    const squareElem = container.querySelector(`[data-squareid="${square}"]`);
-    expect(squareElem.firstChild).not.toHaveStyle(userHighlightSquareStyle);
-  });
-};
-
 // Don't print annoying warnings and errors to the console when running tests
 beforeAll(() => {
+  const error = console.error;
+  const warn = console.warn
   jest.spyOn(console, 'warn').mockImplementation((msg: string, ...rest) => {
     if (msg.startsWith('Warning: componentWillReceiveProps has been renamed')) {
       return;
     } else {
-      console.warn(msg, ...rest);
+      warn(msg, ...rest);
+    }
+  });
+  jest.spyOn(console, 'error').mockImplementation((arg1, arg2, msg, ...rest) => {
+    if (msg.startsWith('Invalid prop `check`')) {
+      return;
+    } else {
+      error(arg1, arg2, msg, ...rest);
     }
   });
 });
@@ -52,149 +42,74 @@ beforeEach(() => {
 describe('<ChessGuide /> in "learn" mode with simple ChessTree', () => {
   const simpleChessTree: ChessTree = makeChessTree(['e4', 'e5', 'Nf3', 'Nc6'], []);
 
-  it('should have a Chessboard component', () => {
-    const { container } = render(<ChessGuide chessTree={simpleChessTree} />);
-    expect(container.querySelector('[data-boardid="0"]')).not.toBeNull();
-  });
-
-  it('should not highlight first move initially', () => {
-    const { container } = render(<ChessGuide chessTree={simpleChessTree} />);
-    expectSquaresNotToBeHighlighted(container, ['e2', 'e4']);
-  });
-
-  it('should highlight first move after short wait', async () => {
-    const { container } = render(<ChessGuide chessTree={simpleChessTree} />);
-    await waitFor(
-      () => {
-        expectSquaresToBeHighlighted(container, ['e2', 'e4']);
-      },
-      { timeout: 2000 },
-    );
-  });
-
-  it('should be able to play first move', () => {
-    const { getByTestId } = render(
+  it('user should be able to play first correct move', async () => {
+    const { getByTestId, getByText } = render(
       <ChessGuide
+        lessonType={LessonType.OPENING}
+        boardSizePixels={500}
         chessTree={simpleChessTree}
         userPlaysAs='white'
         renderExtraControlsForTesting
-      />,
+      >
+        <MovesPane
+          height={500}
+          playedMoves={[]}
+          selectedMoveIdx={null}
+          changeSelectedMoveIdx={(idx) => void idx}
+        />
+      </ChessGuide>
     );
     const playNextMoveBtn = getByTestId(SELECT_BTN_TEST_ID);
     fireEvent.click(playNextMoveBtn);
-    getByTestId('wP-e4');
-  });
-
-  it('should play first move when stepForwardBtn clicked', () => {
-    const { container, getByTestId } = render(<ChessGuide chessTree={simpleChessTree} />);
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    getByTestId('wP-e4');
-  });
-
-  it('should play computer move after user plays first move', async () => {
-    const { getByTestId } = render(
-      <ChessGuide
-        chessTree={simpleChessTree}
-        userPlaysAs='white'
-        renderExtraControlsForTesting
-      />,
-    );
-    const playNextMoveBtn = getByTestId(SELECT_BTN_TEST_ID);
-    fireEvent.click(playNextMoveBtn);
-    getByTestId('wP-e4');
     await waitFor(() => {
-      getByTestId('bP-e5');
+      getByText('e4', { exact: true });
     });
   });
 
-  it('should not play computer move when stepForwardBtn clicked', () => {
-    const { container, getByTestId } = render(<ChessGuide chessTree={simpleChessTree} />);
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    getByTestId('wP-e4');
-    expect(() => getByTestId('bP-e5')).toThrow();
-  });
-
-  it('should play computer move when stepForwardBtn clicked second time', () => {
-    const { container, getByTestId } = render(<ChessGuide chessTree={simpleChessTree} />);
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    fireEvent.click(stepForwardBtn);
-    getByTestId('bP-e5');
-  });
-
-  it('should autoplay when alwaysAutoplay=true and stepForwardBtn clicked', async () => {
-    const { container, getByTestId } = render(
-      <ChessGuide chessTree={simpleChessTree} alwaysAutoplay={true} />,
-    );
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    await waitFor(
-      () => {
-        getByTestId('wP-e4');
-        getByTestId('bP-e5');
-      },
-      { timeout: 1000 },
-    );
-  });
-
-  it('highlights second user move after user plays first move', async () => {
-    const { container, getByTestId } = render(
+  it('should play opponent move after user plays first move', async () => {
+    const { getByText, getByTestId } = render(
       <ChessGuide
+        lessonType={LessonType.OPENING}
+        boardSizePixels={500}
         chessTree={simpleChessTree}
         userPlaysAs='white'
         renderExtraControlsForTesting
-      />,
+      >
+        <MovesPane
+          height={500}
+          playedMoves={[]}
+          selectedMoveIdx={null}
+          changeSelectedMoveIdx={(idx) => void idx}
+        />
+      </ChessGuide>
     );
     const playNextMoveBtn = getByTestId(SELECT_BTN_TEST_ID);
     fireEvent.click(playNextMoveBtn);
-    await waitFor(() => expectSquaresToBeHighlighted(container, ['g1', 'f3']));
+    await waitFor(() => {
+      getByText('e4', { exact: true });
+      getByText('e5', { exact: true });
+    });
   });
 
-  it('highlights second user move after stepForwardBtn clicked twice', async () => {
-    const { container } = render(<ChessGuide chessTree={simpleChessTree} />);
-    const stepForwardBtn = container.querySelector('[aria-label="step forward"]');
-    fireEvent.click(stepForwardBtn);
-    fireEvent.click(stepForwardBtn);
-    await waitFor(() => expectSquaresToBeHighlighted(container, ['g1', 'f3']));
-  });
-
-  it('sets board orientation to black if `userPlaysAs` prop set to black', () => {
-    const { getByTestId } = render(
-      <ChessGuide chessTree={simpleChessTree} userPlaysAs={'black'} />,
+  it('computer immediately plays first move if userPlaysAs black', async () => {
+    const { getByText } = render(
+      <ChessGuide
+        lessonType={LessonType.OPENING}
+        boardSizePixels={500}
+        chessTree={simpleChessTree}
+        userPlaysAs={'black'}
+      >
+        <MovesPane
+          height={500}
+          playedMoves={[]}
+          selectedMoveIdx={null}
+          changeSelectedMoveIdx={(idx) => void idx}
+        />
+      </ChessGuide>
     );
-    const whiteKing = getByTestId('wK-e1');
-    const row: HTMLElement = whiteKing.parentElement.parentElement.parentElement;
-    expect(row.previousSibling).toBeNull();
-  });
-
-  it('sets board orientation to white if `userPlaysAs` prop not set', () => {
-    const { getByTestId } = render(<ChessGuide chessTree={simpleChessTree} />);
-    const whiteKing = getByTestId('wK-e1');
-    const row: HTMLElement = whiteKing.parentElement.parentElement.parentElement;
-    expect(row.nextSibling).toBeNull();
-  });
-
-  it('sets board orientation to white if `userPlaysAs` set to white', () => {
-    const { getByTestId } = render(
-      <ChessGuide chessTree={simpleChessTree} userPlaysAs={'white'} />,
-    );
-    const whiteKing = getByTestId('wK-e1');
-    const row: HTMLElement = whiteKing.parentElement.parentElement.parentElement;
-    expect(row.nextSibling).toBeNull();
-  });
-
-  it('autoplays first move if userPlaysAs="black"', async () => {
-    const { getByTestId } = render(
-      <ChessGuide chessTree={simpleChessTree} userPlaysAs='black' />,
-    );
-    await waitFor(
-      () => {
-        getByTestId('wP-e4');
-      },
-      { timeout: 1000 },
-    );
+    await waitFor(() => {
+      getByText('e4', { exact: true });
+    });
   });
 });
 
@@ -204,13 +119,25 @@ describe('<ChessGuide /> with ChessTree that branches immediately', () => {
     [makeChessTree(['e4', 'e5'], []), makeChessTree(['d4', 'd5'], [])],
   );
 
-  it('highlights both of the first move options', async () => {
-    const { container } = render(<ChessGuide chessTree={chessTree} />);
-    await waitFor(
-      () => {
-        expectSquaresToBeHighlighted(container, ['e2', 'e4', 'd2', 'd4']);
-      },
-      { timeout: 1000 },
+  it('does not play first move automatically', async () => {
+    const { queryByText } = render(
+      <ChessGuide
+        lessonType={LessonType.OPENING}
+        boardSizePixels={500}
+        chessTree={chessTree}
+        userPlaysAs={'black'}
+      >
+        <MovesPane
+          height={500}
+          playedMoves={[]}
+          selectedMoveIdx={null}
+          changeSelectedMoveIdx={(idx) => void idx}
+        />
+      </ChessGuide>
     );
+    await waitFor(() => {
+      expect(queryByText('e4', { exact: true })).toBeNull();
+      expect(queryByText('d4')).toBeNull();
+    });
   });
 });
