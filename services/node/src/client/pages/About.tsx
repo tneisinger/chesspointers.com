537eb8fbd9c7ea6fbbd51545c1ba9373fccc1d91
Tyler Neisinger <tjneisi@gmail.com>
2021-04-15 15:55:53 -0700

    Change about page text

--- services/node/src/client/pages/About.tsx
+++ services/node/src/client/pages/About.tsx
@@ -5,8 +5,15 @@ import Box from '@material-ui/core/Box';
 import Grid from '@material-ui/core/Grid';
 
 const useStyles = makeStyles(() => ({
+  aboutRoot: {
+    maxWidth: '700px',
+    margin: '0 auto',
+  },
   heading: {
-    marginBottom: '1rem',
+    marginBottom: '1.25rem',
+  },
+  bodyText: {
+    fontSize: '1.1rem',
   }
 }));
 
@@ -14,7 +21,7 @@ const AboutPage: React.FunctionComponent = () => {
   const classes = useStyles({});
 
   return (
-    <Grid item xs={12}>
+    <Grid item className={classes.aboutRoot}>
       <Box p={4}>
         <Typography
           className={classes.heading}
@@ -24,8 +31,10 @@ const AboutPage: React.FunctionComponent = () => {
         >
           About
         </Typography>
-        <Typography align='center'>
-          The goal of this site is to help you get better at chess.
+        <Typography className={classes.bodyText}>
+          ChessPointers.com is a passion project made by someone who loves building web
+          apps and studying chess. If you'd like to get in touch, kindly send an email to
+          <a href='mailto: chesspointers@gmail.com'> chesspointers@gmail.com</a>.
         </Typography>
       </Box>
     </Grid>
