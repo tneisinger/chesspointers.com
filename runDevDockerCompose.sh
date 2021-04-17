1561b083f2b52e0c93640218cc33da8f03bb512a
Tyler Neisinger <tjneisi@gmail.com>
2021-04-17 00:06:23 -0700

    Give different container names to prod vs dev
    Assign the production version and the development version their own
    container names, thereby creating unique containers for each environment
    type. Also, turn on TYPEORM_SYNCRONIZE in prod so that the database can
    be set up. I may want to turn that back off at some point.

--- runDevDockerCompose.sh
+++ runDevDockerCompose.sh
@@ -3,5 +3,8 @@
 # make docker-compose files and bring in the config variables
 . ./.makeDockerComposeFiles.sh
 
+# Set the name of the docker-compose project, noting that it is DEV
+export COMPOSE_PROJECT_NAME="ChessPointers_dev"
+
 # run docker-compose using the DOCKER_COMPOSE_FILE from config.sh
 docker-compose -f $DOCKER_COMPOSE_BASE_FILE -f $DOCKER_COMPOSE_DEV_FILE $@
--- runProdDockerCompose.sh
+++ runProdDockerCompose.sh
@@ -3,5 +3,8 @@
 # make docker-compose files and bring in the config variables
 . ./.makeDockerComposeFiles.sh
 
+# Set the name of the docker-compose project, noting that it is PROD
+export COMPOSE_PROJECT_NAME="ChessPointers_prod"
+
 # run docker-compose using the DOCKER_COMPOSE_FILE from config.sh
 docker-compose -f $DOCKER_COMPOSE_BASE_FILE -f $DOCKER_COMPOSE_PROD_FILE $@
