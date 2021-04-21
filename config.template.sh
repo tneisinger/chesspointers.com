eabe63baa91ed3814d3866416db9574a11ac0733
Tyler Neisinger <tjneisi@gmail.com>
2021-04-21 14:59:49 -0700

    Update the config template file

--- config.template.sh
+++ config.template.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 # Use this file as a template to create your own "config.sh" file
 
 # Database info - fill in with your values
@@ -8,16 +7,26 @@ DATABASE_HOST=yourDatabaseHost
 DATABASE_USERNAME=yourDatabaseUsername
 DATABASE_PASSWORD=yourDatabasePassword
 DATABASE_NAME=yourDatabaseName
-DATABASE_PORT=yourDatabasePort
 
-# The values below shouldn't be changed
 
-DOCKER_COMPOSE_FILE=".docker-compose.yaml"
-DOCKER_COMPOSE_TEMPLATE="docker-compose.template.yaml"
+# You shouldn't have to change the values below
+
+DATABASE_PORT=5432
+
+DOCKER_COMPOSE_BASE_FILE=".docker-compose-base.yaml"
+DOCKER_COMPOSE_BASE_TEMPLATE="docker-compose-base.template.yaml"
+
+DOCKER_COMPOSE_DEV_FILE=".docker-compose-dev.yaml"
+DOCKER_COMPOSE_DEV_TEMPLATE="docker-compose-dev.template.yaml"
+
+DOCKER_COMPOSE_PROD_FILE=".docker-compose-prod.yaml"
+DOCKER_COMPOSE_PROD_TEMPLATE="docker-compose-prod.template.yaml"
 
 NODE_DIR="./services/node"
-NODE_DOCKERFILE_TEMPLATE="${NODE_DIR}/Dockerfile.template"
-NODE_DOCKERFILE="${NODE_DIR}/.Dockerfile"
+NODE_DEV_DOCKERFILE_TEMPLATE="${NODE_DIR}/Dockerfile-dev.template"
+NODE_DEV_DOCKERFILE="${NODE_DIR}/.Dockerfile-dev"
+NODE_PROD_DOCKERFILE_TEMPLATE="${NODE_DIR}/Dockerfile-prod.template"
+NODE_PROD_DOCKERFILE="${NODE_DIR}/.Dockerfile-prod"
 
 CLIENT_DIR="${NODE_DIR}/src/client"
 CLIENT_DOTENV_TEMPLATE="${CLIENT_DIR}/.env.template"
